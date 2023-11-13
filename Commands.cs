50a523a66df02fe7a6c3a5d46cf5bc12c66a217a
Даниил Ковалев <dovalevk@gmail.com>
2023-11-13 19:05:57 +0700

    Изменены варианты обработки данных на сервере

--- Commands.cs
+++ Commands.cs
@@ -16,7 +16,7 @@ namespace AISLab2
 {
     class Commands
     {
-
+        static Logger logger = LogManager.GetCurrentClassLogger();
         public static string ReceiveData(Socket udpSocket, EndPoint senderEndPoint)
         {
             try
@@ -59,278 +59,52 @@ namespace AISLab2
         }
 
 
-
-        public static async Task<string> FullOutputAsync(Socket udpSocket, EndPoint senderEndPoint, StudentContext db)
-        {
-            List<Student> students = await GetStudents(db);
-            if (students.Count == 0)
-                return "Нет доступных записей в базе данных.";
-
-            string response = "Список студентов:\n";
-            foreach (Student student in students)
-            {
-                string gender = student.Gender ? "Мужской" : "Женский";
-                response += $"\nId: {student.Id}\nФИО: {student.LastName} {student.FirstName} {student.MiddleName}\nПол: {gender}\nДата рождения: {student.Birthday:yyyy-MM-dd}\n";
-            }
-            return response;
-        }
-
-        public static async Task<string> OutputByIdAsync(Socket udpSocket, EndPoint senderEndPoint, StudentContext db)
-        {
-            string response = "Введите Id студента: ";
-            await SendDataAsync(udpSocket, senderEndPoint, response);
-            string studentId = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-
-            if (!int.TryParse(studentId, out int id))
-                return "Некорректный ввод Id.";
-
-            var student = await db.Students.FirstOrDefaultAsync(s => s.Id == id);
-            if (student != null)
-            {
-                string gender = student.Gender ? "Мужской" : "Женский";
-                response = $"Id: {student.Id}\nФИО: {student.LastName} {student.FirstName} {student.MiddleName}\nПол: {gender}\nДата рождения: {student.Birthday:yyyy-MM-dd}\n";
-            }
-            else 
-                response = "Студент с указанным Id не найден.";
-
-            return response;
-        }
-
-        public static async Task<string> SaveRecordsAsync(Socket udpSocket, EndPoint senderEndPoint, StudentContext db)
+        public static async void SendListOfStudents(Socket udpSocket, EndPoint senderEndPoint, StudentContext db)
         {
             try
             {
-                await db.SaveChangesAsync();
-                return "Изменения сохранены в базе данных.";
+                List<Student> students = db.Students.ToList();
+                await SendDataAsync(udpSocket, senderEndPoint, JsonSerializer.Serialize(students));
+                logger.Info("Cписок студентов отправлен клиенту.");
             }
             catch (Exception ex)
             {
-                return $"Ошибка при сохранении данных: {ex.Message}";
+                logger.Error($"Ошибка при отправке списка студентов: {ex.Message}");
             }
         }
 
-        public static async Task<string> DeleteRecordByIdAsync(Socket udpSocket, EndPoint senderEndPoint, StudentContext db)
+        public static async Task ReceiveDataForWriteAsync(Socket udpSocket, EndPoint senderEndPoint, StudentContext db)
         {
-            await SendDataAsync(udpSocket, senderEndPoint, "Введите Id студента для удаления: ");
-            var studentId = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-
-            if (!int.TryParse(studentId, out int id))
-                return "Некорректный ввод Id.";
-
-            var student = await db.Students.FirstOrDefaultAsync(s => s.Id == id);
-            if (student != null)
+            string receivedStudent = await ReceiveDataAsync(udpSocket, senderEndPoint);
+            try
             {
-                DeleteStudent(id, db);
-                await db.SaveChangesAsync();
-                return $"Студент с Id {id} удален из базы данных.";
+                AddStudent(JsonSerializer.Deserialize<Student>(receivedStudent), db);
+                logger.Info("Студент успешно добавлен.");
             }
-            else
+            catch(Exception ex)
             {
-                return "Студент с указанным Id не найден.";
+                logger.Error($"Ошибка при добавлении студента: {ex.Message}");
             }
         }
 
-        public static async Task<string> AddRecordAsync(Socket udpSocket, EndPoint senderEndPoint, StudentContext db)
+        public static async Task ReceiveDataForDelete(Socket udpSocket, EndPoint senderEndPoint, StudentContext db)
         {
-            await SendDataAsync(udpSocket, senderEndPoint, "Введите Id студента: ");
-            var studentId = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-
-            if (!int.TryParse(studentId, out int id))
-                return "Некорректный ввод Id.";
-
-            await SendDataAsync(udpSocket, senderEndPoint, "Введите имя студента: ");
-            var firstName = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-
-            await SendDataAsync(udpSocket, senderEndPoint, "Введите фамилию студента: ");
-            var lastName = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-
-            await SendDataAsync(udpSocket, senderEndPoint, "Введите Отчество студента: ");
-            var middleName = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-
-            await SendDataAsync(udpSocket, senderEndPoint, "Введите Пол студента (Мужской/Женский): ");
-            var genderInput = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-            var isMale = genderInput.Trim().Equals("Мужской", StringComparison.OrdinalIgnoreCase);
-
-            await SendDataAsync(udpSocket, senderEndPoint, "Введите Дату рождения студента: ");
-            var birthdateInput = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-            if (DateTime.TryParse(birthdateInput, out DateTime birthdate))
+            string receivedIndex = await ReceiveDataAsync(udpSocket, senderEndPoint);
+            try
             {
-                var newStudent = new Student
+                if(int.TryParse(receivedIndex, out int index))
                 {
-                    Id = id,
-                    FirstName = firstName,
-                    LastName = lastName,
-                    MiddleName = middleName,
-                    Gender = isMale,
-                    Birthday = birthdate
-                };
-
-                AddStudent(newStudent, db);
-                await db.SaveChangesAsync();
-
-                return "Новая запись студента добавлена.";
-            }
-            else
-            {
-                return "Некорректный формат даты. Введите дату в формате ГГГГ-ММ-ДД.";
-            }
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-        #region 2
-        //public static async void SendListOfStudents(Socket udpSocket, EndPoint senderEndPoint, StudentContext db)
-        //{
-        //    try
-        //    {
-        //      //  Console.WriteLine("Вызван метод FullOutputAsync");
-        //        List<Student> students = db.Students.ToList();
-        //        await SendDataAsync(udpSocket, senderEndPoint, JsonSerializer.Serialize(students));
-        //    }
-        //    catch(Exception ex)
-        //    {
-        //        Console.WriteLine($"Ошибка при вызове метода FullOutputAsync: , {ex.Message} ");
-        //    }
-        //}
-
-        //public static async Task ReceiveDataForAddAsync(Socket udpSocket, EndPoint senderEndpoint, StudentContext db)
-        //{
-        //    string receivedStudent = await ReceiveDataAsync(udpSocket, senderEndpoint);
-        //    try
-        //    {
-        //        AddStudent(JsonSerializer.Deserialize<Student>(receivedStudent), db);
-        //        Console.WriteLine("Запись добавлена");
-        //    }
-        //    catch(Exception ex)
-        //    {
-        //        Console.WriteLine($"Ошибка при вызове метода ReceiveDataForAddAsync , {ex.Message} ");
-        //    }
-        //}
-
-        //public static async Task RecieveDataForDeleteAsync(Socket udpSocket, EndPoint senderEndPoint, StudentContext db)
-        //{
-        //    string receivedIndex = await ReceiveDataAsync(udpSocket, senderEndPoint);
-        //    try
-        //    {
-        //        if (int.TryParse(receivedIndex, out int index))
-        //        {
-        //            DeleteStudent(index, db);
-        //            Console.WriteLine($"Удален студент с id = {index}");
-        //        }
-        //        else Console.WriteLine("Ошибка ввода id");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine($"Ошибка при вызове метода RecieveDataForDeleteAsync , {ex.Message} ");
-        //    }
-        //}
-
-        /*
-        public static async Task<string> FullOutputAsync(List<Student> Students)
-        {
-            Console.WriteLine("Вызван метод FullOutputAsync");
-            return await Task.Run(() => FullOutput(Students));
-        }
-        /////////
-        public static async Task<string> OutputByIdAsync(Socket udpSocket, EndPoint senderEndPoint, List<Student> Students)
-        {
-            Console.WriteLine("Вызван метод OutputByIdAsync");
-            string serverAnswer;
-            string data;
-            serverAnswer = "\nВведите ID: ";
-            await SendDataAsync(udpSocket, senderEndPoint, serverAnswer);
-            data = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-            if (Check.UintCheck(data))
-            {
-                return await Task.Run(() => OutputById(Students, Convert.ToUInt32(data)).ToString());
-            }
-            return "\nОшибка.Студента с данным ID не существует";
-        }
-        public static async Task<string> SaveRecordsAsync(List<Student> Students, string filePath) 
-        {
-            Console.WriteLine("Вызван метод SaveRecordsAsync");
-            return await Task.Run(() => SaveRecords(Students, filePath));
-        }
-
-        public static async Task<string> DropRecordAsync(Socket udpSocket, EndPoint senderEndPoint, List<Student> Students) 
-        {
-            Console.WriteLine("Вызван метод DropRecordAsync");
-            string serverAnswer;
-            string data;
-            serverAnswer = "\nВведите ID: ";
-            await SendDataAsync(udpSocket, senderEndPoint, serverAnswer);
-            data = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-            if (Check.UintCheck(data))
-            {
-                return await Task.Run(() => DropRecord(Students, Convert.ToUInt32(data)).ToString());
-            }
-            return "\nОшибка.Студента с данным ID не существует";
-        }
-
-        public static async Task<string> AddRecordAsync(Socket udpSocket, EndPoint senderEndPoint, List<Student> Students)
-        {
-            Console.WriteLine("Вызван метод AddRecordAsync");
-            string serverAnswer;
-            string data;
-
-            do
-            {
-                serverAnswer = "\nВведите ID: ";
-                await SendDataAsync(udpSocket, senderEndPoint, serverAnswer);
-                data = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-
-                if (Check.UintCheck(data))
-                {
-                    serverAnswer = "\nВведите имя: ";
-                    await SendDataAsync(udpSocket, senderEndPoint, serverAnswer);
-                    string varfirstname = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-
-                    serverAnswer = "\nВведите фамилию: ";
-                    await SendDataAsync(udpSocket, senderEndPoint, serverAnswer);
-                    string varlastname = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-
-                    serverAnswer = "\nВведите Отчество: ";
-                    await SendDataAsync(udpSocket, senderEndPoint, serverAnswer);
-                    string varmiddlename = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-
-                    serverAnswer = "\nВведите пол (0 - Женский, 1 - Мужской): ";
-                    await SendDataAsync(udpSocket, senderEndPoint, serverAnswer);
-                    string genderInput = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-                    bool vargender = Check.BoolCheck(genderInput);
-
-                    serverAnswer = "\nВведите дату рождения (ГГГГ-ММ-ДД): ";
-                    await SendDataAsync(udpSocket, senderEndPoint, serverAnswer);
-                    string varbirthday = ReceiveDataAsync(udpSocket, senderEndPoint).Result;
-
-                    if (Check.IsAllLetters(varfirstname) && Check.IsAllLetters(varlastname) && Check.IsAllLetters(varmiddlename) && Check.IsDateValid(varbirthday))
-                    {
-                        Student varstudent = new Student(Convert.ToUInt32(data), varfirstname, varlastname, varmiddlename, vargender, varbirthday);
-                        Students.Add(varstudent);
-                        return "\nЗапись успешно добавлена.";
-                    }
-                    else
-                    {
-                        return "\nОшибка. Введите корректные данные и попробуйте снова.";
-                    }
+                    DeleteStudent(index, db);
+                    logger.Info($"Студент c Id {index} успешно удален.");
                 }
                 else
-                {
-                    return "\nОшибка. Введите корректный ID и попробуйте снова.";
-                }
+                    logger.Error($"Ошибка при удалении студента.");
             }
-            while (true);
-      
+            catch(Exception ex)
+            {
+                logger.Error($"Ошибка при удалении студента: {ex.Message}");
+            }
         }
-          */
-        #endregion
+
     }
 }
